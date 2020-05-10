bc889fbd61403e77c1edd17758d6755535ae8a22
Yu Harada <hoge@yahoo.co.jp>
2020-05-10 15:47:14 +0900

    フォントを変更

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatefulWidget{
 
 class MyHomePageState extends State<MyHomePage>{
   int totalFee = 0;
-  var rateList = [1.0, 1.0];
+  var rateList = [1.0, 1.5];
   var numberList = [0, 0];
   var feePerPersonList = [0,0];
 
@@ -57,89 +57,108 @@ class MyHomePageState extends State<MyHomePage>{
       feePerPersonList[i] = (feePerPerson * rateList[i]).ceil();
     }
   }
+  var descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 20.0,
+    );
 
   @override 
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("傾斜つき割り勘計算"),),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 64.0),
-            child:
-              Row(children: <Widget>[
-                Text("合計金額"),
-                Flexible(child:
-                  TextField(
-                    onChanged: onChangedTatalFee,
-                    keyboardType: TextInputType.number),
-                ),
-                Text("円"),
-              ],),
-          ),
-          Column(children: <Widget>[
+      body: DefaultTextStyle.merge(
+        style:descTextStyle,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 64.0),
+              padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 64.0),
               child:
-              Row(children: <Widget>[
-                Text("傾斜比率"),
-                Flexible(child:
-                  TextField(
-                    onChanged: (value){
-                      onChangedRate(value, 0);
-                    },
-                    keyboardType: TextInputType.number),
-                ),
-                Text("倍"),
-                Flexible(child:
-                  TextField(
-                    onChanged: (value){
-                      onChangedNumber(value, 0);
-                    },
-                    keyboardType: TextInputType.number),
-                ),
-                Text("人")
-              ],),
+                Row(children: <Widget>[
+                  Text("合計金額"),
+                  Flexible(child:
+                    TextField(
+                      onChanged: onChangedTatalFee,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number),
+                  ),
+                  Text("円"),
+                ],),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 64.0),
-              child:
+            Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 64.0),
+                child:
                 Row(children: <Widget>[
                   Text("傾斜比率"),
                   Flexible(child:
-                    TextField(
+                    TextFormField(
+                      initialValue: rateList[0].toString(),
+                      textAlign: TextAlign.center,
                       onChanged: (value){
-                        onChangedRate(value, 1);
+                        onChangedRate(value, 0);
                       },
                       keyboardType: TextInputType.number),
                   ),
                   Text("倍"),
                   Flexible(child:
-                    TextField(
+                    TextFormField(
+                      initialValue: numberList[1].toString(),
+                      textAlign: TextAlign.center,
                       onChanged: (value){
-                        onChangedNumber(value, 1);
+                        onChangedNumber(value, 0);
                       },
                       keyboardType: TextInputType.number),
                   ),
                   Text("人")
                 ],),
-            ),
-          ],),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child:
-              Center(
-                child:
-                Column(children: <Widget>[
-                  Text("支払い金額"),
-                  Text("${rateList[0]}倍の人 ${feePerPersonList[0]}"),
-                  Text("${rateList[1]}倍の人 ${feePerPersonList[1]}円"),
-                ],)
               ),
-          ),
-        ],)
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 64.0),
+                child:
+                  Row(children: <Widget>[
+                    Text("傾斜比率"),
+                    Flexible(child:
+                      TextFormField(
+                        initialValue: rateList[1].toString(),
+                        textAlign: TextAlign.center,
+                        onChanged: (value){
+                          onChangedRate(value, 1);
+                        },
+                        keyboardType: TextInputType.number),
+                    ),
+                    Text("倍"),
+                    Flexible(child:
+                      TextFormField(
+                        initialValue: numberList[1].toString(),
+                        textAlign: TextAlign.center,
+                        onChanged: (value){
+                          onChangedNumber(value, 1);
+                        },
+                        keyboardType: TextInputType.number),
+                    ),
+                    Text("人")
+                  ],),
+              ),
+            ],),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child:
+                Center(
+                  child:
+                  Column(children: <Widget>[
+                    Text("支払い金額"),
+                    Text("${rateList[0]}倍の人 ${feePerPersonList[0]}円"),
+                    Text("${rateList[1]}倍の人 ${feePerPersonList[1]}円"),
+                  ],)
+                ),
+            ),
+          ],)
+      )
     );
   }
 }
